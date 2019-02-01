42f12245fde683c979f582e55e671fd01e99d1ff
Onno Haldar <onno@zgw.nu>
2019-02-01 18:43:03 +0100

    refactor

--- projects/ng-joint/src/lib/shapes/standard/rectangle/standard-rectangle.component.ts
+++ projects/ng-joint/src/lib/shapes/standard/rectangle/standard-rectangle.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { GenericElementShapeComponent } from '../../shapes';
 import { NgJointStandardElement } from '../shapes-standard';
@@ -12,14 +12,36 @@ import { StandardRectangle } from './standard-rectangle';
     `,
 })
 export class StandardRectangleComponent extends GenericElementShapeComponent {
-  @Input() body: {};
-  @Input() label: {};
+  /** two-way binding body property */
+  @Input() get body() { return this._body; }
+  @Output() bodyChange = new EventEmitter<{}>();
+
+  /** two-way binding label property */
+  @Input() get label() { return this._label; }
+  @Output() labelChange = new EventEmitter<{}>();
 
   constructor(private standardRectangleService: StandardRectangleService) {
     super(standardRectangleService);
   }
 
+  private _body: {};
+  private _label: {};
   shape: StandardRectangle;
+
+  set body(bodyValue: {}) {
+    if (this._body !== bodyValue) {
+      this._body = bodyValue;
+      this.yChange.emit(this._body);
+    }
+  }
+
+  set label(labelValue: {}) {
+    if (this._label !== labelValue) {
+      this._label = labelValue;
+      this.labelChange.emit(this._label);
+    }
+  }
+
 
 }
 
