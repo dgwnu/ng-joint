7caeb93ca4de4376030783322c708ad9f67fba3e
Onno Haldar <onno@zgw.nu>
2019-02-01 16:19:17 +0100

    refactor

--- projects/ng-joint/src/lib/shapes/standard/rectangle/standard-rectangle.service.ts
+++ projects/ng-joint/src/lib/shapes/standard/rectangle/standard-rectangle.service.ts
@@ -37,36 +37,7 @@ export class StandardRectangleService implements ElementShapeService {
   }
 
   setChanges(changes: SimpleChanges, component: StandardRectangleComponent) {
-    const shape = component.shape;
-    if (!shape) { return; } // first time changes is before shape is created
-
-    const bbox = shape.element.getBBox();
-
-    // detect position change
-    let positionChangeDetected = false;
-    if (changes.x) {
-      if (changes.x.currentValue !== bbox.x) { positionChangeDetected = true; }
-    } else if (changes.y) {
-      if (changes.y.currentValue !== bbox.y) { positionChangeDetected = true; }
-    }
-    // detect size change
-    let sizeChangeDetected = false;
-    if (changes.width) {
-      if (changes.width.currentValue !== bbox.width) { sizeChangeDetected = true; }
-    } else if (changes.height) {
-      if (changes.height.currentValue !== bbox.height) { sizeChangeDetected = true; }
-    }
-
-    // process detected changes
-    if (positionChangeDetected) {
-        component.shape.element.position(component.x, component.y);
-        console.log('onShapeChanges.position');
-    }
-    if (sizeChangeDetected) {
-        component.shape.element.resize(component.width, component.height);
-        console.log('onShapeChanges.resize');
-    }
-
+    this.service.setChanges(changes, component);
   }
 
 }
