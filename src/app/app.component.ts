9719f6ebf45b0020e1a7e26cf0db109d8e4cef6c
Onno Haldar <onno@zgw.nu>
2019-02-07 13:45:19 +0100

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,4 @@ export class AppComponent {
     { path: 'struct-dir-demo', label: 'Structural Directives Demo'},
     { path: 'uml-class-diagram-demo', label: 'UML Class Diagram Demo'}
   ];
-  // activeLink = this.navLinks[0];
 }
