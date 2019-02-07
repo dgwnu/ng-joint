855eeb2fe5814465cd04de7772ee0f605c3f817d
Onno Haldar <onno@zgw.nu>
2019-02-07 15:18:47 +0100

    refactor added doc iframe to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { AppMaterialModule } from './app-material.module';
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     AppMaterialModule,
     HelloWorldDemoModule,
     AngularElementsDemoModule,
--- src/app/docs/docs.module.ts
+++ src/app/docs/docs.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { DocsComponent } from './docs.component';
 import { DocsRoutingModule } from './docs-routing.module';
 
 @NgModule({
   declarations: [ DocsComponent ],
-  imports: [ DocsRoutingModule ],
+  imports: [ DocsRoutingModule, FlexLayoutModule ],
   exports: [ DocsComponent, DocsRoutingModule ]
 })
 export class DocsModule { }
