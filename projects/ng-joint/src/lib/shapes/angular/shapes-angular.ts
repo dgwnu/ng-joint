de83994471284addeb4b5ee4b60a58a1b225586c
Onno Haldar <onno@zgw.nu>
2019-02-21 12:38:18 +0100

    refactor

--- projects/ng-joint/src/lib/shapes/angular/shapes-angular.ts
+++ projects/ng-joint/src/lib/shapes/angular/shapes-angular.ts
@@ -1,7 +1,29 @@
-import { DiaElement } from '../../dia/element';
-import {  NgJointElement } from '../shapes';
+import { ElementRef } from '@angular/core';
 
-export abstract class AngularElementShape extends DiaElement {
+import { dia, shapes } from 'jointjs';
+
+import { DiaElement } from '../../dia/element';
+import { NgJointElement } from '../shapes';
+
+
+export class AngularElementShape extends DiaElement {
+    private _ngElementRef: ElementRef;
+
+    constructor(id: string, options: dia.Element.GenericAttributes<shapes.basic.RectSelectors>) {
+        super({ id: id, options });
+
+        options.attrs = {
+            rect: { stroke: 'none', 'fill-opacity': 0 },
+        };
+
+        this._jointjsObject = new shapes.basic.Rect(options);
+    }
+
+    set ngElementRef(value: ElementRef) {
+        this._ngElementRef = value;
+    }
+
+    get ngNode(): Node { return this._ngElementRef.nativeElement; }
 
 }
 
