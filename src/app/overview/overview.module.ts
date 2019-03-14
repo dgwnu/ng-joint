54287f6830784c32ed66344ac839f1effbc1b1f0
Onno Haldar <onno@zgw.nu>
2019-03-15 00:31:35 +0100

    refactor

--- src/app/overview/overview.module.ts
+++ src/app/overview/overview.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatListModule, MatIconModule } from '@angular/material';
 
 import { HowToUseDocModule } from '../how-to-use-doc';
 import { OverviewComponent } from './overview.component';
@@ -9,6 +10,7 @@ import { OverviewRoutingModule } from './overview-routing.module';
   declarations: [OverviewComponent],
   imports: [
     CommonModule,
+    MatListModule, MatIconModule,
     HowToUseDocModule,
     OverviewRoutingModule],
   exports: [OverviewRoutingModule]
