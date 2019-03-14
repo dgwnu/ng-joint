4d6a79b8b4325b730198e48b1675b9a3d63d8c44
Onno Haldar <onno@zgw.nu>
2019-03-15 00:06:54 +0100

    refactor

--- src/app/how-to-use-doc/how-to-use-doc-examples/how-to-use-doc-examples.component.ts
+++ src/app/how-to-use-doc/how-to-use-doc-examples/how-to-use-doc-examples.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-how-to-use-doc-examples',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./how-to-use-doc-examples.component.scss']
 })
 export class HowToUseDocExamplesComponent implements OnInit {
+  @Input() exmaples: TemplateRef<any>;
 
   constructor() { }
 
--- src/app/how-to-use-doc/how-to-use-doc-overview/how-to-use-doc-overview.component.ts
+++ src/app/how-to-use-doc/how-to-use-doc-overview/how-to-use-doc-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-how-to-use-doc-overview',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./how-to-use-doc-overview.component.scss']
 })
 export class HowToUseDocOverviewComponent implements OnInit {
+  @Input() overview: TemplateRef<any>;
 
   constructor() { }
 
